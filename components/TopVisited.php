091c7472c32c738871e3f1978e4e3846c2c02430
Eduardo Cabrera <ivan.cabrera@havas-se.com>
2017-01-29 21:18:58 -0600

    commit inicial, el plugin es funcional, no está listo, pero ya jala bien

---
+++ components/TopVisited.php
@@ -0,0 +1,188 @@
+<?php namespace PolloZen\MostVisited\Components;
+
+use Cms\Classes\Page;
+use Cms\Classes\ComponentBase;
+use Carbon\Carbon;
+use PolloZen\MostVisited\Models\Visits;
+use RainLab\Blog\Models\Category;
+use RainLab\Blog\Models\Post;
+
+class TopVisited extends ComponentBase
+{
+     /**
+     * @var Illuminate\Database\Eloquent\Collection | array
+     */
+    public $topPosts;
+
+    /**
+     * Reference to the page name for linking to posts.
+     * @var string
+     */
+    // public $postPage;
+
+    /**
+     *
+     */
+    // public $dateRange;
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Top Visited Component',
+            'description' => 'Retrieve the top visited RainLab Blog Posts'
+        ];
+    }
+    /**
+     * Definition of propertys
+     * @return [array]
+     */
+    public function defineProperties()
+    {
+        return [
+            'period' =>[
+                'title'         => 'Top 10 from:',
+                'description'   => '',
+                'default'       => 4,
+                'type'          => 'dropdown',
+                'options' => [
+                    '1' => 'Today',
+                    '2' => 'Current week',
+                    '3' => 'Yesterday',
+                    '4' => 'Last week'
+                ],
+                'showExternalParam' => false
+            ],
+            'postPerPage' => [
+                'title'         => 'Post per page',
+                'description'   => '',
+            ],
+            'category' =>[
+                'title'         => 'Category',
+                'description'   => 'Filter result by category. All categories by default',
+                'type'          => 'dropdown',
+                'placeholder'   => 'Select a category',
+                'showExternalParam' => false
+            ],
+            'postPage' => [
+                'title'         => 'Post page',
+                'description'   => 'Page to show linked posts',
+                'type'          => 'dropdown',
+                'default'       => 'blog/post',
+                'group'         => 'Links',
+            ],
+            'slug' => [
+                'title'         => 'rainlab.blog::lang.settings.post_slug',
+                'description'   => 'rainlab.blog::lang.settings.post_slug_description',
+                'default'       => '{{ :slug }}',
+                'type'          => 'string',
+                'group'         => 'Links'
+            ]
+        ];
+    }
+    /**
+     * prepare Vars function
+     * @return [object]
+     */
+    protected function prepareVars()
+    {
+        $this->postParam = $this->page['postParam'] = $this->property('postParam');
+    }
+    /**
+     * [getPostPageOptions]
+     * @return [array][Blog]
+     */
+    public function getPostPageOptions()
+    {
+        return Page::sortBy('baseFileName')->lists('baseFileName', 'baseFileName');
+    }
+    /**
+     * [getCategoryOptions]
+     * @return [array list] [Blog Categories]
+     */
+    public function getCategoryOptions(){
+        $categories =  BlogCategory::orderBy('name')->lists('name','id');
+        return $categories;
+    }
+    public function onRun(){
+        $this->prepareVars();
+
+        /*Get the category filter*/
+        $category = $this->property('category') ? $this->property('category') : null;
+
+        /* Get post page */
+        $postPage = $this->property('postPage') ? $this->property('postPage') : '404';
+
+        /*Get date range*/
+        switch($this->property('period')){
+            case '1':
+                $dateRange = Carbon::today();
+            break;
+            case '2':
+                $fromDate = Carbon::now()->startOfWeek()->format('Y-m-d');
+                $toDate = Carbon::now()->endOfWeek()->format('Y-m-d');
+            break;
+            case '3':
+                $dateRange = Carbon::yesterday();
+            break;
+            case '4':
+                $fromDate = Carbon::now()->subDays(7)->startOfWeek()->format('Y/m/d');
+                $toDate = Carbon::now()->subDays(7)->endOfWeek()->format('Y/m/d');
+            break;
+            default:
+                $dateRange = Carbon::today();
+            break;
+        }
+
+        // $query = Post::isPublished()
+        //     ->whereIn('id', function($q){
+        //         switch($this->property('period')){
+        //             case '1':
+        //                 $dateRange = Carbon::today();
+        //             break;
+        //             case '2':
+        //                 $fromDate = Carbon::now()->startOfWeek()->format('Y-m-d');
+        //                 $toDate = Carbon::now()->endOfWeek()->format('Y-m-d');
+        //             break;
+        //             case '3':
+        //                 $dateRange = Carbon::yesterday();
+        //             break;
+        //             case '4':
+        //                 $fromDate = Carbon::now()->subDays(7)->startOfWeek()->format('Y/m/d');
+        //                 $toDate = Carbon::now()->subDays(7)->endOfWeek()->format('Y/m/d');
+        //             break;
+        //             default:
+        //                 $dateRange = Carbon::today();
+        //             break;
+        //         }
+        //         $q = isset($dateRange) ? $q->where('date',$dateRange) : $q->whereBetween('date', array($fromDate, $toDate));
+        //         $q  ->select('post_id')
+        //             ->from(with(new Visits)->getTable())
+        //             ->selectRaw('sum(visits) as visits')
+        //             ->groupBy('post_id')
+        //             ->orderBy('visits','desc')
+        //             ->get();
+        //     })
+        //     ->limit(10);
+        // $topPost = $query ->get();
+
+        /**
+         * Get topPost y los demás heredados
+         */
+        $query = new Visits;
+        $query = isset($dateRange) ? $query->where('date',$dateRange) : $query->whereBetween('date', array($fromDate, $toDate));
+        $query  ->select('post_id')
+                ->selectRaw('sum(visits) as visits, count(post_id) as touchs')
+                ->groupBy('post_id')
+                ->orderBy('visits','desc')
+                ->limit($this->property('postPerPage'));
+        $topPost = $query->get();
+
+
+        /* Add a "url" helper attribute for linking to each post */
+        // $topPost->each(function($query) {
+        //    $query->setUrl($this->postPage,$this->controller);
+        // });
+
+        $this->topPosts = $topPost;
+    }
+}
---
+++ models/Visits.php
@@ -0,0 +1,35 @@
+<?php namespace PolloZen\MostVisited\Models;
+
+use Model;
+use RainLab\Blog\Models\Category as BlogCategory;
+use RainLab\Blog\Models\Post as BlogPost;
+
+/**
+ * Visits Model
+ */
+class Visits extends Model
+{
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'pollozen_mostvisited_visits';
+
+    /**
+     * @var array Guarded fields
+     */
+    protected $guarded = ['*'];
+
+    /**
+     * @var array Fillable fields
+     */
+    protected $fillable = ['post_id','date'];
+
+    /**
+     * @var array Relations
+     */
+
+    public $belongsTo = [
+        'post' => ['Rainlab\Blog\Models\Post']
+    ];
+}
